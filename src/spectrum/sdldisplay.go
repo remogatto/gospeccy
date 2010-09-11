0262659fe1bdc09901a4b5cc8b1a4b650bb8b898
Andrea Fazzi <andrea.fazzi@alcacoop.it>
2010-09-11 22:35:57 +0200

    Revert the value receiver in SDLSurface methods

--- src/spectrum/sdldisplay.go
+++ src/spectrum/sdldisplay.go
@@ -40,31 +40,31 @@ type SDLSurface struct {
 	surface *sdl.Surface
 }
 
-func (s *SDLSurface) Width() uint {
+func (s SDLSurface) Width() uint {
 	return uint(s.surface.W)
 }
 
-func (s *SDLSurface) Height() uint {
+func (s SDLSurface) Height() uint {
 	return uint(s.surface.H)
 }
 
-func (s *SDLSurface) Bpp() uint {
+func (s SDLSurface) Bpp() uint {
 	return uint(s.surface.Format.BytesPerPixel)
 }
 
-func (s *SDLSurface) Pitch() uint {
+func (s SDLSurface) Pitch() uint {
 	return uint(s.surface.Pitch)
 }
 
 // Return the address of pixel at (x,y)
-func (s *SDLSurface) addrXY(x, y uint) uintptr {
+func (s SDLSurface) addrXY(x, y uint) uintptr {
 	pixels := uintptr(unsafe.Pointer(s.surface.Pixels))
 	offset := uintptr(y*s.Pitch() + x*s.Bpp())
 
 	return uintptr(unsafe.Pointer(pixels + offset))
 }
 
-func (s *SDLSurface) setPixel(addr uintptr, color uint32) {
+func (s SDLSurface) setPixel(addr uintptr, color uint32) {
 	*(*uint32)(unsafe.Pointer(addr)) = color
 }
 

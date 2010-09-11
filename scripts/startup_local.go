eb09c4f467ac143d6ac436a7c8bac60ee7eb2ef4
⚛ <0xe2.0x9a.0x9b@gmail.com>
2010-09-11 21:57:03 +0200

    Remove scripts/startup_local.go from the GIT repositoty

--- scripts/startup_local.go
+++
@@ -1,7 +0,0 @@
-// This Go script is executed on GoSpeccy startup,
-// immediately after the "startup.go" script.
-//
-// You can put your own startup actions here:
-// change the display scale, disable sound, load a snapshot, etc
-
-
